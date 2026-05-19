b079fe5ac016d97b4540295a0223b79a821144a4
Lu1s-m0r415 <luismmorais16@gmail.com>
2026-05-19 13:55:03 -0300

    Create Função pra calcular o Delta.cpp

---
+++ Função pra calcular o Delta.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+float calc_delta(float a, float b, float c)
+{
+	float delta;
+	delta = (pow(b, 2)) - (4 * a * c);
+	return delta;
+}
+
+int main()
+{
+	float a;
+	float b;
+	float c;
+	float delta = 0;
+	
+	cin >> a >> b >> c;
+	
+	delta = calc_delta(a, b, c);
+	
+	cout << "delta = " << delta << endl;
+	
+	return 0;
+}

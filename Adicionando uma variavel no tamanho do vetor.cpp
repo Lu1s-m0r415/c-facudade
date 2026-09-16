d2a7774b7df12ea5489e5c89710b1afde22fb204
Lu1s-m0r415 <luismmorais16@gmail.com>
2026-09-16 13:48:11 -0300

    Create Adicionando uma variavel no tamanho do vetor.cpp

---
+++ Adicionando uma variavel no tamanho do vetor.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int N;             // tamanho do vetor
+	int *vetor = NULL; // ponteiro para o vetor
+	int i;
+	
+	// Lendo tamanho desejado
+	cin >> N;
+	
+	// Alocando memoria para o vetor
+	vetor = new int[N];
+	
+	// lendo o vetor
+	for(i = 0; i < N; i++)
+		cin >> vetor[i];
+	
+	for(i = 0; i < N; i++)
+		cout << vetor[i] << endl;
+		
+	return 0;
+}
